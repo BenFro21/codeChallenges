59d306745ad716108a29ceca1459dcc263506abd
benfro21 <broad0601@gmail.com>
2022-10-28 20:09:52 -0400

    First Commit

---
+++ practice.js
@@ -0,0 +1,63 @@
+// Bianry serch Tree 
+
+// You are given a list of numbers stored in a list, A. Your challenge is to build a [Binary Search Tree] (Binary search tree - Wikipedia 959) to store these numbers. You will need to:
+
+// 1 Represent each node of the tree (including its data, left child and right child).
+// 2 Print the tree out in an understandable form.
+// 3 Make a function to insert a list of numbers (A) into the tree.
+// 4 Check if you can insert all the numbers in A into your tree, and that it prints out as expected.
+// help: https://en.wikipedia.org/wiki/Binary_search_tree 
+
+
+
+
+
+// Reverse Words 
+// Write a function that will take a given string 181 and reverse the order of the words. “Hello world” becomes “world Hello” and “May the Fourth be with you” becomes “you with be Fourth the May”
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write a function, fibonacciFinder50, that will find the 50th number in the Fibonacci Sequence.
+
+// Function Name: fibonacciFinder50
+// Input: There are no parameters to this function
+// Output: an integer, the 50th number in the Fibonacci Sequence
+// Example: fibonacciFinder50() => 7778742049
+// In the Fibonacci Sequence 101, the first two numbers are 0 and 1 and every number thereafter is the sum of the previous two numbers in the sequence
+// For this challenge, consider 0 to be the 1st Fibonacci Number, not the 0th (i.e. do not zero-index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write a function, primeNumberDetector, that tests if a number, n is a prime number.
+
+// Function Name: primeNumberDetector
+// Input: integer n
+// Output: boolean: true if n is a prime number else false.
+// Example: primeNumberDetector(13) => True
