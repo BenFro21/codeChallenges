6d34ad41b077445d01d66ec62f816f66e2579a44
benfro21 <broad0601@gmail.com>
2022-12-08 16:07:48 -0500

    practice

--- practice.js
+++ practice.js
@@ -398,3 +398,17 @@ var maxSubArray = function(nums) {
  var merge = function(intervals) {
     
 };
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////0th
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal 
+// substring
+// consisting of non-space characters only.
+
+var lengthOfLastWord = function(s) {
+    // Split into an array find length of the array 
+    let newArr = s.trim().split(' ')
+    return newArr[newArr.length -1].length
+
+};
