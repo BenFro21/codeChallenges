249eac528c91005b72044b32df7b41c138067d77
benfro21 <broad0601@gmail.com>
2023-01-19 15:18:34 -0500

    practice

--- javascriptYoutube.js
+++ javascriptYoutube.js
@@ -315,3 +315,19 @@ const urlIfy = (str, n) => {
 }
 // console.log(urlIfy("My name Is Ben", 14))
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    
+};
