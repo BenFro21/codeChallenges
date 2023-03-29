f503e9a3df8f304d18a421d1aaac2e0cd97027b6
benfro21 <broad0601@gmail.com>
2023-03-29 16:44:25 -0400

    practice/ documentation

--- fundamentaljs.js
+++ fundamentaljs.js
@@ -9,33 +9,7 @@
 
 
 
-// 125. Write a JavaScript program to convert a float-point arithmetic to the Decimal mark form and It will make a comma separated string from a number. Go to the editor
 
-// Click me to see the solution
-
-// 126. Write a JavaScript program to create a specified currency formatting from a given number. Go to the editor
-
-// Click me to see the solution
-
-// 127. Write a JavaScript program to Iterate over a callback n times. Go to the editor
-
-// Click me to see the solution
-
-// 128. Write a JavaScript program to get removed elements of a given array until the passed function returns true. Go to the editor
-
-// Click me to see the solution
-
-// 129. Write a JavaScript program to get removed elements from the end of a given array until the passed function returns true. Go to the editor
-
-// Click me to see the solution
-
-// 130. Write a JavaScript program to remove n elements from the end of a given array. Go to the editor
-
-// Click me to see the solution
-
-// 131. Write a JavaScript program to get an array with n elements removed from the beginning from a given array Go to the editor
-
-// Click me to see the solution
 
 // 132. Write a JavaScript program to get the symmetric difference between two given arrays, using a provided function as a comparator. Go to the editor
 
