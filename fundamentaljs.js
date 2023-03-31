05992fbde217a40d713ff6edb43c2a643e69877a
benfro21 <broad0601@gmail.com>
2023-03-31 17:05:32 -0400

    Practice

--- fundamentaljs.js
+++ fundamentaljs.js
@@ -11,16 +11,3 @@
 
 
 
-// 132. Write a JavaScript program to get the symmetric difference between two given arrays, using a provided function as a comparator. Go to the editor
-
-// Click me to see the solution
-
-// 133. Write a JavaScript program to get the symmetric difference between two given arrays, after applying the provided function to each array element of both. Go to the editor
-
-// Click me to see the solution
-
-// 134. Write a JavaScript program to get the symmetric difference between two given arrays. Go to the editor
-
-// Click me to see the solution
-
-// 135. Write a JavaScript program to get the sum of the powers of all the numbers from start to end (both inclusive)
