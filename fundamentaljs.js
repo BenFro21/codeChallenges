e41c8e64896c58b2fbc5b9cac8df212f2338869d
benfro21 <broad0601@gmail.com>
2023-04-02 13:47:56 -0400

    practice

--- fundamentaljs.js
+++ fundamentaljs.js
@@ -1,13 +1,2 @@
 
-// Click me to see the solution
-
-// 103. Write a JavaScript program to convert a given string into an array of words. Go to the editor
-
-// Click me to see the solution
-
-// 104. Write a JavaScript program to test a value, x, against a predicate function. If true, return fn(x). Else, return x. Go to the editor
-
-
-
-
 
